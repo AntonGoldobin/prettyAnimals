1e0772c4de9ac4e0c07be723a2e52d4dc77593b5
AntonGoldobin <722451@mail.ru>
2023-02-24 21:40:49 +0700

    Schedule with envs url

--- src/modules/schedule.ts
+++ src/modules/schedule.ts
@@ -6,7 +6,7 @@ export const schedule = (config: Config) => {
 	console.log('schedule has been started')
 	cron.scheduleJob(config.cron, () => {
 		axios
-			.post(`http://${process.env.SHORTS_SERVER}/youtube-shorts`, config)
+			.post(`${process.env.SHORTS_SERVER}/youtube-shorts`, config)
 			.then((res) => {
 				console.log('post request has been sended')
 			})
@@ -19,7 +19,7 @@ export const schedule = (config: Config) => {
 export const scheduleOnce = (config: Config) => {
 	console.log('schedule runs only once and immediately')
 	axios
-		.post('http://localhost:3000/youtube-shorts', config)
+		.post(`${process.env.SHORTS_SERVER}/youtube-shorts`, config)
 		.then((res) => {
 			console.log('post request has been sended')
 		})
